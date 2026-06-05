2d1fe84a7cb9f18f1c5e1fc83994290dd6e0cdfb
Dnyaneshwar Kasurde <dnyaneshwarkasurde38@gmail.com>
2026-06-05 14:38:02 +0530

    Create Linked_List_Cycle_II.cpp

---
+++ Slow-Fast_Pointer/Linked_List_Cycle_II.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    #define ed endl
+    #define Node ListNode
+    #define null NULL
+    ListNode *detectCycle(ListNode *head) {
+        Node* slow=head;
+        Node* fast=head;
+
+         while(fast != null && fast->next!=null){
+            slow= slow->next;
+            fast = fast->next->next;
+            if(slow==fast){
+                slow = head;
+                while(slow != fast){
+                    slow=slow->next;
+                    fast=fast->next;
+                }
+                return slow;
+            }
+            
+        }
+        
+    return 0; }
+};
