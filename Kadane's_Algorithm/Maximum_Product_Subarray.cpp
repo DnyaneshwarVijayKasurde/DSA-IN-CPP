3cba8caa8017ca609a216034069e86f5c734adf1
Dnyaneshwar Kasurde <dnyaneshwarkasurde38@gmail.com>
2026-06-08 21:32:27 +0530

    Create Maximum_Product_Subarray.cpp

---
+++ Kadane's_Algorithm/Maximum_Product_Subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int i=0;
+        int maxend=nums[0];
+        int minend=nums[0];
+        int res=nums[0];
+        for(i=1;i<nums.size();i++){
+            int v1=nums[i];
+            int v2=minend * nums[i];
+            int v3=maxend * nums[i];
+            maxend=max(v1,max(v2,v3));
+            minend=min(v1,min(v2,v3));
+            res=max(res,max(maxend,minend));
+        }
+        return res;
+    }
+};
